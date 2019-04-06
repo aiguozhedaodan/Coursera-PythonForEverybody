2e38403a7d1456cc815a736485a7e58cdf1b9ee0
Shibo <ssb@hust.edu.cn>
2019-04-06 23:55:25 +0200

    Create assignment.py

---
+++ c3w5/assignment.py
@@ -0,0 +1,22 @@
+import urllib.request, urllib.parse, urllib.error
+import xml.etree.ElementTree as ET
+import ssl
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter location: ')
+print('Retrieving', url)
+xml = urllib.request.urlopen(url, context=ctx)
+data = xml.read() #If without this line, it will shows:TypeError: a bytes-like object is required, not 'HTTPResponse'
+stuff = ET.fromstring(data)
+lst = stuff.findall('comments/comment')
+print('Count:', len(lst))
+all = 0
+number = 0
+for item in lst:
+    print(item.find('count').text)
+    #number = item.find('count').text)
+    all = all + int(number)
+print('Sum:',sum)
